f3a11c47e455d9648d21a78f571b08fa997edecb
matej.tuzil <matej.tuzil@csystem.cz>
2023-03-23 16:04:32 +0000

    Add methods for work with app db

--- server/app.py
+++ server/app.py
@@ -1,75 +1,70 @@
-import redis
-import random
 from datetime import datetime
-from faker import Faker
+import redis
+
+from generate_data import generate_data
+
+from redisearch import Client, TextField, NumericField, IndexDefinition
 
 
-redis_client = redis.Redis(host="localhost", port=6379, db=0)
+class App:
+    def __init__(self, redis_client):
+        self.redis_client = redis_client
 
+    def get_all_messages_for_room(self, room_code, page=1, page_size=10):
+        """Get all messages for a room"""
+        start = (page - 1) * 10
+        end = start + page_size - 1
+        message_ids = self.redis_client.zrevrange(
+            f"room:{room_code}:messages", start, end
+        )
+        return [self.get_message(message_id) for message_id in message_ids]
 
-def generate_data(num_users=10, num_rooms=2, num_messages_per_room=100):
-    """Generate data for chat app - uses redis to store data
-    generate 100 users with faker
-    generate 2 rooms with faker
-    generate 1000 messages with faker
-    """
-    fake = Faker()
+    def get_message(self, message_id):
+        """Get a message by id"""
+        return self.redis_client.hgetall(f"message:{message_id}")
 
-    # Generate users
-    users = {}
-    for i in range(num_users):
-        user_id = i + 1
-        username = fake.user_name()
-        first_name = fake.first_name()
-        last_name = fake.last_name()
-        email = fake.email()
-        user = {
-            "id": user_id,
-            "username": username,
-            "first_name": first_name,
-            "last_name": last_name,
-            "email": email,
+    def get_all_user_rooms(self, user_id):
+        """Get all rooms for a user"""
+        return self.redis_client.smembers(f"user:{user_id}:rooms")
+
+    def get_room_members(self, room_code):
+        """Get all members for a room"""
+        return self.redis_client.smembers(f"room:{room_code}:users")
+
+    def add_message_to_room(self, room_code, sender_id, text):
+        """Add a message to a room"""
+        message_id = self.redis_client.incr("next_message_id")
+        message = {
+            "id": message_id,
+            "sender_id": sender_id,
+            "timestamp": datetime.timestamp(datetime.now()),
+            "text": text,
         }
-        users[user_id] = user
-
-        # Save user to Redis
-        redis_client.hset(f"user:{user_id}", mapping=user)
-
-    # Generate rooms
-    room_codes = ["room" + str(i) for i in range(1, num_rooms + 1)]
-    rooms = {}
-    for i in range(num_rooms):
-        room_code = room_codes[i]
-        room_users = random.sample(
-            range(1, num_users + 1), 10
-        )  # select 10 random users for this room
-        room = {"code": room_code, "users": room_users}
-        rooms[room_code] = room
-
-        # Save room to Redis
-        redis_client.sadd(f"room:{room_code}:users", *room_users)
-
-    # Generate messages for each room
-    for room_code, room in rooms.items():
-        for i in range(num_messages_per_room):
-            sender_id = random.choice(room["users"])
-            message_id = redis_client.incr("next_message_id")
-            message = {
-                "sender_id": sender_id,
-                "text": fake.text(),
-                "timestamp": datetime.timestamp(datetime.now()),
-                "id": message_id,
-            }
-
-            redis_client.zadd(
-                f"room:{room_code}:messages", {message_id: message["timestamp"]}
-            )
-            redis_client.hset(f"messages:{message['id']}", mapping=message)
-
-    # Close Redis connection
-    redis_client.close()
+        self.redis_client.hset(f"message:{message_id}", mapping=message)
+        self.redis_client.zadd(
+            f"room:{room_code}:messages", {message_id: message["timestamp"]}
+        )
+        return message
 
 
 if __name__ == "__main__":
+    redis_client = redis.Redis(host="localhost", port=6379, db=0)
     redis_client.flushall()
-    generate_data(100, 2, 5000)
+    generate_data(redis_client, 10, 2, 5000)
+
+    app = App(redis_client)
+
+    # client = Client("messages", conn=redis_client)
+
+    # # IndexDefinition is available for RediSearch 2.0+
+    # definition = IndexDefinition(prefix="messages:")
+
+    # # Creating the index definition and schema
+    # try:
+    #     client.create_index((TextField("text")), definition=definition)
+    # except:
+    #     pass
+
+    # res = client.search("art")
+    # print(res.total)
+    # print(res.docs)
